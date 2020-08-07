a1d969a622a3d9268d5a592e81551bdac1f1fdce
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-06 23:08:21 -0300

    refactor jest.config

--- __test__/jest.config.js
+++ __test__/jest.config.js
@@ -1,13 +1,13 @@
-import { resolve } from "path";
-const root = resolve(__dirname, "..");
+import { resolve } from 'path';
+const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
 export default {
   ...rootConfig,
   ...{
     rootDir: root,
-    displayName: "end2end-tests",
-    setupFilesAfterEnv: ["<rootDir>/__test__/jest-setup.ts"],
-    testMatch: ["<rootDir>/__test__/**/*.test.ts"],
+    displayName: 'end2end-tests',
+    setupFilesAfterEnv: ['<rootDir>/__test__/jest-setup.ts'],
+    testMatch: ['<rootDir>/__test__/**/*.test.ts'],
   },
 };
