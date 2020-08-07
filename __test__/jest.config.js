9c54709615ca6b3a01f060881c0c4756834892d5
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-07 00:26:13 -0300

    refactor forecast test to pass and customizing global.d.ts with new variable

--- __test__/jest.config.js
+++ __test__/jest.config.js
@@ -1,13 +1,10 @@
-import { resolve } from 'path';
+const { resolve } = require('path');
 const root = resolve(__dirname, '..');
 const rootConfig = require(`${root}/jest.config.js`);
 
-export default {
-  ...rootConfig,
-  ...{
-    rootDir: root,
-    displayName: 'end2end-tests',
-    setupFilesAfterEnv: ['<rootDir>/__test__/jest-setup.ts'],
-    testMatch: ['<rootDir>/__test__/**/*.test.ts'],
-  },
-};
+module.exports = {...rootConfig, ...{
+  rootDir: root,
+  displayName: 'end2end-tests',
+  setupFilesAfterEnv: ['<rootDir>/__test__/jest-setup.ts'],
+  testMatch: ['<rootDir>/__test__/**/*.test.ts'],
+}}
