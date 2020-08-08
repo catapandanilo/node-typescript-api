d9d87464198c2a040008ccb1a5d265a91a075c35
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-08 15:16:17 -0300

    refactor StormGlass with normalizeResponse and interfaces

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,7 @@
-import { StormClass } from '@src/clients/stormGlass';
+import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
@@ -8,10 +10,10 @@ describe('StormClass client', () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    axios.get = jest.fn().mockReturnValue({});
+    axios.get = jest.fn().mockReturnValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormClass(axios);
+    const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({})
+    expect(response).toEqual(stormGlassNormalized3HoursFixture)
   });
 });
