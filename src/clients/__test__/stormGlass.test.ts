89c4fb0c8a5a727b6b8a88b1de677b0d0957510c
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-08-08 14:39:15 -0300

    use AxiosStatic to request stormglass

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,11 +1,16 @@
 import { StormClass } from '@src/clients/stormGlass';
+import axios from 'axios'
+
+jest.mock('axios');
 
 describe('StormClass client', () => {
   test('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    const stormGlass = new StormClass();
+    axios.get = jest.fn().mockReturnValue({});
+
+    const stormGlass = new StormClass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual({})
   });
